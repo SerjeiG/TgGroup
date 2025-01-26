8f802f60f9ba1fa0bde1b253837b98a12be5a1bd
totgeometr <probkavokne@gmail.com>
2025-01-26 15:28:35 +0700

    maria

---
+++ maria.py
@@ -0,0 +1,12 @@
+
+
+
+
+def hello(message, bot):
+    text = message.text
+    message.text = ""
+    if message.text == "привет":
+        bot.send_message(message.chat.id, text="привет!")
+    if message.text == "пока":
+        bot.send_message(message.chat.id, text="до встречи!")
+
