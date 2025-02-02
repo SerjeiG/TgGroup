4b82444265c3eb1aa69edd4604e6b38a9bd2a25b
mariyafeayas <feayaserig@gmail.com>
2025-02-02 15:17:01 +0700

    modiefied main.py, add def hello

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import telebot
 
 import config
 
+import maria
+
+
 admins = [1215240396]
 clients = []
 #инициализировать бота, команда старт, переменные по хранению пользователя (id)
@@ -17,12 +20,7 @@ def info(message):
 
 @bot.message_handler(commands= ['hello'])
 def hello(message):
-    text = message.text
-    message.text = ""
-    if "привет" in message:
-        bot.send_message(message.chat.id, text="привет!")
-    if "пока" in message:
-        bot.send_message(message.chat.id, text="до встречи!")
+    maria.hello(message, bot)
 
 bot.infinity_polling()
 
